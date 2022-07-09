bb2447b493c6b8bf7c8f2759655bfdac015557aa
Tosibinaronmuro <tosironj@gmail.com>
2022-07-09 20:26:01 +0100

    fixed a tiny isue

--- Components/Drawer.jsx
+++ Components/Drawer.jsx
@@ -52,7 +52,7 @@ const DrawerComp = () => {
 </div></Link>
 
 {/* SKILLS */}
-<Link href="/#skills"   scroll={true}><div   className='bg-black flex text-white rounded-full w-auto h-auto border shadow-md  my-5 p-2  justify-start'>
+<Link href="/#skills"   scroll={true}><div onClick={onClose}  className='bg-black flex text-white rounded-full w-auto h-auto border shadow-md  my-5 p-2  justify-start'>
 <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z"></path></svg>  <span className='ml-5'>SKILLS</span>
 </div></Link> 
 {/* projects */}
